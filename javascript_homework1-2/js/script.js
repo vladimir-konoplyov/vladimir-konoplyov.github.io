7aaa375a51705eccbfd59fa131e25c5c72bdf0cf
Vladimir Konoplyov <volodimir.konoplyov@ukr.net>
2016-03-20 13:13:49 +0200

    Edit script.js, add vertical spaces

--- javascript_homework1-2/js/script.js
+++ javascript_homework1-2/js/script.js
@@ -1,10 +1,13 @@
 // Задача 1. Возведение числа в степень
 console.log('TASK 1.');
+
 var num = prompt('Enter integer number: ', '');
 var exponent = prompt('Enter exponent: ', '');
+
 function pow() {
     var result, convertExp;
     var numExp = 1;
+
     if (isNaN(num / 2) || isNaN(exponent / 2) || (num =='') || (exponent == '')) {
         return result = 'ATTENTION! You entered not a number! Please, reload this page and enter correct number.';
     } else {
@@ -22,9 +25,12 @@ function pow() {
                 }
             }
         }
-    return result = numExp;
+
+    return result = numExp;    
 }
+
 var numberInExponent = pow();
+
 console.log('Entered number:', num);
 console.log('Entered exponent:', exponent);
 console.log('RESULT = ', numberInExponent);
@@ -32,21 +38,27 @@ console.log('_________________________________');
 
 // Задача 2. Поиск совпадений введенного имени с именами в массиве
 console.log('TASK2.');
+
 var arr = [];
+
 for (var i = 0; i <= 4; i++) {
     arr[i] = prompt('Введите 5 имен: ', '');
 }
+
 var enteredName = prompt('Введите Ваше имя: ', '');
 var proverka;
+
 console.log('Введенные имена: ', arr);
 console.log('Ваше имя: ', enteredName);
+
  for (var j = 0; j <= 4; j++) {
     if ((arr[j] == enteredName) && (arr[j] != '')) {
         alert(arr[j] + ', вы успешно вошли');
         proverka = true;
-        break
+        break;
     }
  }
+
  if (proverka != true) {
     alert('ОШИБКА! Введенное имя не совпадает с существующими именами, либо введено пустое имя!');
  }
