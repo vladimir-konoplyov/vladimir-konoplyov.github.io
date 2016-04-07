282c0bc84b98e865193f6e63144a61046394b7d7
Vladimir Konoplyov <volodimir.konoplyov@ukr.net>
2016-04-07 09:51:24 +0300

    Edit Homework 7-8

--- javascript_homework7-8/js/script.js
+++ javascript_homework7-8/js/script.js
@@ -6,46 +6,77 @@ $(function() {
 
 	var $buttonShow = $('#button-show-help');
 
+	var $tab1 = $('#tab1');
+	var $tab2 = $('#tab2');
+	var $tab3 = $('#tab3');
 
+	// Переключаемые табы
 
-	$firstName.mouseover(function() {
-		// $('#tooltips_firstname').slideToggle('slow');
-		$('#tooltips_firstname').fadeIn('slow');
-	})
-	.mouseleave(function() {
-		$('#tooltips_firstname').fadeOut('slow');
+	$('.tab1-text').show();
+	$tab1.addClass('tab-default');
+
+	$tab1.on('click', function() {
+		$('.tab1-text').show();
+		$(this).removeClass('tab-default').removeClass('tab-unvisited').addClass('tab-visited');
+		$('.tab2-text').hide();
+		$('.tab3-text').hide();
+		$tab2.removeClass('tab-visited').addClass('tab-unvisited');
+		$tab3.removeClass('tab-visited').addClass('tab-unvisited');
 	});
 
-	$lastName.mouseover(function() {
-		$('#tooltips_lastname').fadeIn('slow');
+	$tab2.on('click', function() {
+		$('.tab2-text').show();
+		$(this).removeClass('tab-unvisited').addClass('tab-visited');
+		$('.tab1-text').hide();
+		$('.tab3-text').hide();
+		$tab1.removeClass('tab-visited').addClass('tab-unvisited');
+		$tab3.removeClass('tab-visited').addClass('tab-unvisited');
+	});
+	
+	$tab3.on('click', function() {
+		$('.tab3-text').show();
+		$(this).removeClass('tab-default').removeClass('tab-unvisited').addClass('tab-visited');
+		$('.tab1-text').hide();
+		$('.tab2-text').hide();
+		$tab1.removeClass('tab-visited').addClass('tab-unvisited');
+		$tab2.removeClass('tab-visited').addClass('tab-unvisited');
+	});
+
+	// Всплывающие подсказки
+
+	$firstName.mouseover(function() {
+		$('#tooltips_firstname').stop().fadeTo('slow', 1);
 	})
 	.mouseleave(function() {
-		$('#tooltips_lastname').fadeOut('slow');
+		$('#tooltips_firstname').stop().fadeTo('slow', 0);
+	});
+
+
+	$lastName.mouseover(function() {
+		$('#tooltips_lastname').stop().fadeTo('slow', 1);
+	})
+	.mouseleave(function() {
+		$('#tooltips_lastname').stop().fadeTo('slow', 0);
 	});
 
 
 	$address.mouseover(function() {
-		$('#tooltips_address').fadeIn('slow');
+		$('#tooltips_address').stop().fadeTo('slow', 1);
 	})
 	.mouseleave(function() {
-		$('#tooltips_address').fadeOut('slow');
+		$('#tooltips_address').stop().fadeTo('slow', 0);
 	});
 
 	$buttonShow.on('click', function() {
-		$('#tooltips_firstname').fadeIn('slow');
-		$('#tooltips_lastname').fadeIn('slow');
-		$('#tooltips_address').fadeIn('slow');
+		$('#tooltips_firstname').fadeTo('slow', 1);
+		$('#tooltips_lastname').fadeTo('slow', 1);
+		$('#tooltips_address').fadeTo('slow', 1);
 	});
 
 	$buttonShow.mouseleave(function() {
-		$('#tooltips_firstname').fadeOut(3000);
-		$('#tooltips_lastname').fadeOut(3000);
-		$('#tooltips_address').fadeOut(3000);
+		$('#tooltips_firstname').fadeOut(2000);
+		$('#tooltips_lastname').fadeOut(2000);
+		$('#tooltips_address').fadeOut(2000);
 	});
-
-
-
-
-
 
 });
