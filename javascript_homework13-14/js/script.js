79a7804548ce86d707e65555186376c7d1550a13
Vladimir Konoplyov <volodimir.konoplyov@ukr.net>
2016-04-27 21:26:32 +0300

    Edit 2 script.js. Change indentation from tab to space

--- javascript_homework13-14/js/script.js
+++ javascript_homework13-14/js/script.js
@@ -5,95 +5,95 @@ $(function() {
     // Template
 
     var html = $('#output-test').html();
-	var info = [
-		
-		{
-		questionTitle: '1. Что такое HTML? ',
-		questionText: ['Язык гипертекстовой разметки', 'Язык программирования Hybrid', 'Язык программирования C#'],
-		questionAnswer: ['true', 'false', 'false']
-		},
+    var info = [
+        
+        {
+        questionTitle: '1. Что такое HTML? ',
+        questionText: ['Язык гипертекстовой разметки', 'Язык программирования Hybrid', 'Язык программирования C#'],
+        questionAnswer: ['true', 'false', 'false']
+        },
 
-		{
-		questionTitle: '2. Как расшифровывается абревиатура CSS?',
-		questionText: ['Cascading Site Style', 'Cascading Style Sheets', 'Cascad Sheet Style'],
-		questionAnswer: ['false', 'true', 'false']
-		},
+        {
+        questionTitle: '2. Как расшифровывается абревиатура CSS?',
+        questionText: ['Cascading Site Style', 'Cascading Style Sheets', 'Cascad Sheet Style'],
+        questionAnswer: ['false', 'true', 'false']
+        },
 
-		{
-		questionTitle: '3. Кто самый крутой чувак в GoIT? :-)',
-		questionText: ['Тарас Банный', 'Олег Змиюк', 'Андрей Пивоваров'],
-		questionAnswer: ['true', 'true', 'true']
-		}		
-	];
+        {
+        questionTitle: '3. Кто самый крутой чувак в GoIT? :-)',
+        questionText: ['Тарас Банный', 'Олег Змиюк', 'Андрей Пивоваров'],
+        questionAnswer: ['true', 'true', 'true']
+        }       
+    ];
 
-	var testInLocalStorage = JSON.stringify(info);
-	localStorage.setItem('keyTest', testInLocalStorage);
+    var testInLocalStorage = JSON.stringify(info);
+    localStorage.setItem('keyTest', testInLocalStorage);
 
-	var testOutFromLocalStorage = localStorage.getItem('keyTest');
+    var testOutFromLocalStorage = localStorage.getItem('keyTest');
 
-	var variableTest = JSON.parse(testOutFromLocalStorage);
+    var variableTest = JSON.parse(testOutFromLocalStorage);
 
-	var content = tmpl(html, {
-		data: variableTest
-	});
+    var content = tmpl(html, {
+        data: variableTest
+    });
 
-	var answerResult = [];
-	var count = 0,  markPositive = 0, markNegative = 0;
+    var answerResult = [];
+    var count = 0,  markPositive = 0, markNegative = 0;
 
-	$('body').append(content);
+    $('body').append(content);
 
-	$('.output-wrapper').slideDown('slow');
+    $('.output-wrapper').slideDown('slow');
 
-	$('.button-refer').on('click', function() {
+    $('.button-refer').on('click', function() {
 
-		for (var i =  0; i < variableTest.length; i++) {
+        for (var i =  0; i < variableTest.length; i++) {
 
-			for (var j =  0; j < variableTest[i].questionText.length; j++) {
+            for (var j =  0; j < variableTest[i].questionText.length; j++) {
 
-				if (($('#checkbox_' + i + '_' + j).prop('checked')) && (variableTest[i].questionAnswer[j] === 'true')) { 
+                if (($('#checkbox_' + i + '_' + j).prop('checked')) && (variableTest[i].questionAnswer[j] === 'true')) { 
 
-        			answerResult[count] = '<p class="modal-window__output-result">' +'Ответ ' + (i + 1) + '.' + (j + 1) + ' верный' + '</p>';
-        			count++;
-        			markPositive++;
+                    answerResult[count] = '<p class="modal-window__output-result">' +'Ответ ' + (i + 1) + '.' + (j + 1) + ' верный' + '</p>';
+                    count++;
+                    markPositive++;
 
-				} else if (($('#checkbox_' + i + '_' + j).prop('checked')) && (variableTest[i].questionAnswer[j] === 'false')) {
+                } else if (($('#checkbox_' + i + '_' + j).prop('checked')) && (variableTest[i].questionAnswer[j] === 'false')) {
 
-						answerResult[count] = '<p class="modal-window__output-result">' + 'Ответ ' + (i + 1) + '.' + (j + 1) + ' не верный!'+ '</p>';
-						count++;
-						markNegative++;
-						}
-			}			
-		}
+                        answerResult[count] = '<p class="modal-window__output-result">' + 'Ответ ' + (i + 1) + '.' + (j + 1) + ' не верный!'+ '</p>';
+                        count++;
+                        markNegative++;
+                        }
+            }           
+        }
 
-		for (var m = 0; m < answerResult.length; m++) {
-			console.log(answerResult[m]);
-			
-		};
-		console.log('--------------');
-		console.log('Правильных ответов: ', markPositive);
-		console.log('Не правильных ответов: ', markNegative);
+        for (var m = 0; m < answerResult.length; m++) {
+            console.log(answerResult[m]);
+            
+        };
+        console.log('--------------');
+        console.log('Правильных ответов: ', markPositive);
+        console.log('Не правильных ответов: ', markNegative);
 
-		$('.modal-window').slideDown('slowly');
+        $('.modal-window').slideDown('slowly');
 
-		$('div.modal-window__result-here').append(answerResult);
+        $('div.modal-window__result-here').append(answerResult);
 
-		if (markPositive == 5) {
-			 $('div.modal-window__result-here').after('<p class="modal-window__output-result-all-correct">' + 'Поздравляю! Вы ответили правильно на все вопросы!' + '</p>');
-		} else if ((markPositive >= 1) && (markPositive < 5)) {
-			$('div.modal-window__result-here').after('<p class="modal-window__output-result-not-all-correct">' + 'Вы ответили правильно не на все вопросы!' + '</p>');
-			$('p.modal-window__output-result-not-all-correct').after('<div class="button-refer-position"><input class="modal-window__button-retry" type="button" value="Попробовать еще раз"></div>');
-		} else if ((markPositive == 0) && (markNegative ==0))  {
-			$('div.modal-window__result-here').after('<p class="modal-window__output-result-not-choosed">' + 'Вы не выбрали ни одного ответа!' + '</p>');
-			$('p.modal-window__output-result-not-choosed').after('<div class="button-refer-position"><input class="modal-window__button-retry" type="button" value="Попробовать еще раз"></div>');
-		} else if ((markNegative > 0) && (markPositive == 0))  {
-			$('div.modal-window__result-here').after('<p class="modal-window__output-result-not-choosed">' + 'Все ответы неверные!' + '</p>');
-			$('p.modal-window__output-result-not-choosed').after('<div class="button-refer-position"><input class="modal-window__button-retry" type="button" value="Попробовать еще раз"></div>');
-		};
+        if (markPositive == 5) {
+             $('div.modal-window__result-here').after('<p class="modal-window__output-result-all-correct">' + 'Поздравляю! Вы ответили правильно на все вопросы!' + '</p>');
+        } else if ((markPositive >= 1) && (markPositive < 5)) {
+            $('div.modal-window__result-here').after('<p class="modal-window__output-result-not-all-correct">' + 'Вы ответили правильно не на все вопросы!' + '</p>');
+            $('p.modal-window__output-result-not-all-correct').after('<div class="button-refer-position"><input class="modal-window__button-retry" type="button" value="Попробовать еще раз"></div>');
+        } else if ((markPositive == 0) && (markNegative ==0))  {
+            $('div.modal-window__result-here').after('<p class="modal-window__output-result-not-choosed">' + 'Вы не выбрали ни одного ответа!' + '</p>');
+            $('p.modal-window__output-result-not-choosed').after('<div class="button-refer-position"><input class="modal-window__button-retry" type="button" value="Попробовать еще раз"></div>');
+        } else if ((markNegative > 0) && (markPositive == 0))  {
+            $('div.modal-window__result-here').after('<p class="modal-window__output-result-not-choosed">' + 'Все ответы неверные!' + '</p>');
+            $('p.modal-window__output-result-not-choosed').after('<div class="button-refer-position"><input class="modal-window__button-retry" type="button" value="Попробовать еще раз"></div>');
+        };
 
-		$('.modal-window__button-retry').on('click', function() {
-			window.location.reload();
-		});
+        $('.modal-window__button-retry').on('click', function() {
+            window.location.reload();
+        });
 
-	});
+    });
 
 });
