29a1e531b7229e279c87fcb8fcea4da48e1a9759
Vladimir Konoplyov <volodimir.konoplyov@ukr.net>
2016-04-27 21:22:36 +0300

    Edit script.js. Change indentation from tab to space

--- javascript_homework13-14/js/script.js
+++ javascript_homework13-14/js/script.js
@@ -26,10 +26,6 @@ $(function() {
 		}		
 	];
 
-	// var content = tmpl(html, {
-	// 	data: info
-	// });
-
 	var testInLocalStorage = JSON.stringify(info);
 	localStorage.setItem('keyTest', testInLocalStorage);
 
@@ -41,13 +37,8 @@ $(function() {
 		data: variableTest
 	});
 
-	// var answerResult = [[], [], []];
 	var answerResult = [];
 	var count = 0,  markPositive = 0, markNegative = 0;
-
-
-	// console.log(variableTest);
-	
 
 	$('body').append(content);
 
@@ -60,34 +51,19 @@ $(function() {
 			for (var j =  0; j < variableTest[i].questionText.length; j++) {
 
 				if (($('#checkbox_' + i + '_' + j).prop('checked')) && (variableTest[i].questionAnswer[j] === 'true')) { 
-        			// alert('Выбран ответ ' + i + '_' + j);
 
-        			// alert('Ответ' + (i + 1) + '_' + (j + 1) + ' верный' );
         			answerResult[count] = '<p class="modal-window__output-result">' +'Ответ ' + (i + 1) + '.' + (j + 1) + ' верный' + '</p>';
         			count++;
         			markPositive++;
-			} else if (($('#checkbox_' + i + '_' + j).prop('checked')) && (variableTest[i].questionAnswer[j] === 'false')) {
-				// alert('Ответ' + (i + 1) + '_' + (j + 1) + ' не верный!' );
-				answerResult[count] = '<p class="modal-window__output-result">' + 'Ответ ' + (i + 1) + '.' + (j + 1) + ' не верный!'+ '</p>';
-				count++;
-				markNegative++;
-				// countNegative++;
-			}
 
+				} else if (($('#checkbox_' + i + '_' + j).prop('checked')) && (variableTest[i].questionAnswer[j] === 'false')) {
 
-				// if (variableTest[i].questionAnswer[j] === 'true') {
-				// 	alert('TRUE!');
-				// } else {alert('False!');}
-
-
-				// console.log(variableTest[i].questionAnswer[j]);
-
-			}
-
-			
+						answerResult[count] = '<p class="modal-window__output-result">' + 'Ответ ' + (i + 1) + '.' + (j + 1) + ' не верный!'+ '</p>';
+						count++;
+						markNegative++;
+						}
+			}			
 		}
-		
-
 
 		for (var m = 0; m < answerResult.length; m++) {
 			console.log(answerResult[m]);
@@ -97,19 +73,9 @@ $(function() {
 		console.log('Правильных ответов: ', markPositive);
 		console.log('Не правильных ответов: ', markNegative);
 
-		// var rightAnsweStr = '<p class="modal-window__output-result">' + 'Правильных ответов: ' +  markPositive + '</p>';
-		// var wrongAnsweStr = '<p class="modal-window__output-result">' + 'Не правильных ответов ' +  markNegative + '</p>';
-
-
 		$('.modal-window').slideDown('slowly');
 
-
-		// $('.modal-window').append(answerResult);
-		// $('p.modal-window__title').append('11111111111111111');
-
-		// $('p.modal-window__title').after(answerResult);
 		$('div.modal-window__result-here').append(answerResult);
-
 
 		if (markPositive == 5) {
 			 $('div.modal-window__result-here').after('<p class="modal-window__output-result-all-correct">' + 'Поздравляю! Вы ответили правильно на все вопросы!' + '</p>');
@@ -122,55 +88,12 @@ $(function() {
 		} else if ((markNegative > 0) && (markPositive == 0))  {
 			$('div.modal-window__result-here').after('<p class="modal-window__output-result-not-choosed">' + 'Все ответы неверные!' + '</p>');
 			$('p.modal-window__output-result-not-choosed').after('<div class="button-refer-position"><input class="modal-window__button-retry" type="button" value="Попробовать еще раз"></div>');
-
 		};
 
 		$('.modal-window__button-retry').on('click', function() {
-
 			window.location.reload();
-
-			// $('.modal-window').css('display', 'none');
-			// $('.modal-window').empty();
-			// $('body').append(content);
-
 		});
 
-		// if (markPositive == 5) {
-		// 	 $('.modal-window').append('<p class="modal-window__output-result-all-correct">' + 'Поздравляю! Вы ответили правильно на все вопросы!' + '</p>');
-		// } else if ((markPositive >= 1) && (markPositive < 5) ) {
-		// 	$('.modal-window').append('<p class="modal-window__output-result-not-all-correct">' + 'Вы ответили правильно не на все вопросы!' + '</p>');
-		// 	$('.modal-window').append('<div class="button-refer-position"><input class="modal-window__button-retry" type="button" value="Попробовать еще раз"></div>');
-		// } else if ((markPositive == 0) && (markNegative ==0))  {
-		// 	$('.modal-window').append('<p class="modal-window__output-result-not-choosed">' + 'Вы не выбрали ни одного ответа!' + '</p>');
-		// 	$('.modal-window').append('<div class="button-refer-position"><input class="modal-window__button-retry" type="button" value="Попробовать еще раз"></div>');
-
-		// }
-
-		// console.log(answerResult.length);
-
-
-
-		
-
-
-
-		// console.log(answerResult[0][1]);
-		// console.log('Правильных ответов: ',count);
-		// console.log('Неправильных ответов: ', countNegative);
-
-		// for (var k = 0; k <= answerResult.length; k++) {
-		// 	for (var l = 0; l <= answerResult[k].length; l++) {
-
-		// 	console.log(answerResult[k][l]);
-		// 	};
-			// k++;
-		// };
 	});
-
-
-
-
-
-
 
 });
