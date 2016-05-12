64f23a1035a88fda2acdb5bd75569004014927c7
Vladimir Konoplyov <volodimir.konoplyov@ukr.net>
2016-05-12 19:39:39 +0300

    Add Homework 17-18

---
+++ javascript_homework17-18/gruntfile.js
@@ -0,0 +1,53 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    concat: {
+       js_files: {
+        src: ['js/src/*.js'],
+        dest: 'js/dest/script.min.js',
+        options: {
+          separator: ';'
+        }
+      },
+      css_files: {
+        src: ['css/src/*.css'],
+        dest: 'css/dest/style.min.css',
+        options: {
+          separator: ''
+        }
+      }
+    },
+    uglify: {
+      js_files: {
+        src: ['js/dest/script.min.js'],
+        dest: 'js/dest/script.min.js'
+      }
+    },
+  //  cssmin: {
+  //   target: {
+  //     files: [{
+  //         'css/dest/style.min.css': ['css/src/style.min.css']
+  //     }]
+  //   }
+  // }
+
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'css',
+          src: ['dest/style.min.css'],
+          dest: 'css',
+          ext: '.min.css'
+        }]
+      }
+    } 
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  grunt.registerTask('default', ['concat', 'uglify', 'cssmin']);
+
+};
