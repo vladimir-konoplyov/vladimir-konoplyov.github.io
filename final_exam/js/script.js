edbaaf02f75d730688b7cd97f7779b61da5869b4
Vladimir Konoplyov <volodimir.konoplyov@ukr.net>
2016-05-31 16:13:45 +0300

    Add discover block and footer

--- final_exam/js/script.js
+++ final_exam/js/script.js
@@ -25,19 +25,49 @@ $(function() {
 // $(window).load(windowSize); 
 // $(window).resize(windowSize);
 
-// function windowSize(){
-//     if ($(window).width() >= '930') {
-//         var changeDivPartners2 = $('div#partners__image-box-2');
-//         var changeDivPartners3 = $('div#partners__image-box-3');
-//         changeDivPartners3.insertBefore(changeDivPartners2);
-//     }  
-//     else if ($(window).width() < '930') {
-//         changeDivPartners2.insertBefore(changeDivPartners3);
-//        }
-// }
+function windowSize(){
+  //   var w = $(window).width();
+  //   var elem = $('.out-results').text ();
 
-// $(window).load(windowSize); 
-// $(window).resize(windowSize);
+  //   if ((w >= '320') && (w <= '767')) {
+  //       if ($('.out-results').length !== 0) {
+  //      $('.out-results').css('margin', '0 auto 29px');  
+  //        console.log('elem2 = ', elem);          
+  //       }
+  // }
+  // else if ((w >= '768') && (w <= '939')) {
+  //       if ($('.out-results').length !== 0) {  
+  //   $('.out-results').css('overflow', 'hidden');
+  //   $('.out-results').css('margin', '33px auto 95px'); 
+  //   console.log('elem3 = ', elem);        
+  //       }
+
+  //    if ($('.out-results').length == 1) {
+  //   $('.out-results').css('overflow', 'visible');
+  //   $('.out-results').css('margin', '0px auto 29px');         
+  //       }
+
+        // if (!$.trim($('.out-results').val())) {
+        //     $('.out-results').addClass('active');
+        //     $('.active').css('overflow', 'hidden');
+        //     $('.active').css('margin', '33px auto 95px');          
+        // } 
+        // else if ($.trim($('.out-results').val())) {
+        //     $('.out-results').removeClass('active');
+        //     $('.out-results').css('margin', '0 auto 29px');
+        //     $('.active').css('overflow', 'none');
+        // } 
+
+
+
+    // $('.out-results:empty').css('overflow', 'hidden');
+    // $('.out-results').css('margin', '33px auto 95px');
+  // }
+
+}
+
+$(window).load(windowSize); 
+$(window).resize(windowSize);
 
 
     $('.jcarousel1').jcarousel();
@@ -64,6 +94,53 @@ $(function() {
     $('.jcarousel-next3').click(function() {
     $('.jcarousel3').jcarousel('scroll', '+=1');
     });
+
+
+    $('.grid').masonry({
+      // options
+      itemSelector: '.grid-item',
+      columnWidth: 200
+    });
+
+
+
+
+
+        $('.search-form').on('submit', function() {
+        var enteredQuery = $('#entered-query').val();
+        $('.discover__boxes').hide();
+        $.ajax({
+            url: "http://api.pixplorer.co.uk/image?word="+ enteredQuery +"&amount=7&size=tb",
+            // dataType: 'jsonp',
+            // method : 'GET',
+            success: function(data, textStatus) {
+                console.log('Success: ', data);
+                // var ul = document.createElement('ul');
+                // var ul = document.createElement('ul');
+                var divBox = document.createElement('div');
+                divBox.classList.add('out-results__wrapper');
+                $.each(data.images, function(i) {
+                    // var li = document.createElement('li');
+                    // var box = document.createElement('div');
+                    // var divBox = document.createElement('div');
+                    // li.classList.add('output-items');
+                    // li.innerHTML = 'Result №'+ (i+1) + ': ' +'<a href="'+data.images[i].imageurl+'" title="'+data.images[i].imageurl+'" target="_blank">'+data.images[i].word+ '</br>' +'<img class="image-output" src="'+data.images[i].imageurl+'">';
+                    // li.innerHTML = '<img class="image-output" src="'+data.images[i].imageurl+'">';
+                    // li.innerHTML = '<div class="box-image-output' + i + '"><img class="image-output' + i + '" src="'+data.images[i].imageurl+'"></div>';
+                     var outResultsBox = document.createElement('div');
+                     outResultsBox.classList.add('out-results__box'+ i);
+
+                     outResultsBox.innerHTML = '<img class="out-results__image' + i + '" src="'+data.images[i].imageurl+'">';
+                    divBox.appendChild(outResultsBox);                   
+                });
+                $('#out-results').html(divBox);
+            },
+            error: function(data, textStatus) {
+                console.log('Error: ', data);
+            }
+        });    
+        return false;  
+    }); 
 
 
 
