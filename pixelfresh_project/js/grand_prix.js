0d9ddc1cadd091b23f241021416d42ca265ffe3e
vladimir-konoplyov <volodimir.konoplyov@ukr.net>
2016-11-06 10:53:46 +0200

    Add resize function for social buttons

--- pixelfresh_project/js/grand_prix.js
+++ pixelfresh_project/js/grand_prix.js
@@ -11,4 +11,21 @@ $(function() {
     $('.logo__text2').addClass('logo__text2--active');
 
 
+function windowSize() {
+ if ($(window).width() < '480') {
+  $('.at-icon-wrapper').css('line-height', '20px').css('height', '20px').css('width', '20px');
+  $('.at-icon').css('height', '20px').css('width', '20px');
+
+ }
+
+  if ($(window).width() >= '480') {
+  $('.at-icon-wrapper').css('line-height', '32px').css('height', '32px').css('width', '32px');
+  $('.at-icon').css('height', '32px').css('width', '32px');
+ }
+}
+
+$(window).load(windowSize); 
+$(window).resize(windowSize);
+
+
 });
